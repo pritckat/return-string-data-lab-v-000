87d2bff9b5ccc1c83176aaf1a5694e2fef747aa1
Katrina Pritchard <pritckat@gmail.com>
2018-10-25 01:33:37 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price)
+    params.require(:product).permit(:name, :price, :description)
   end
 end
