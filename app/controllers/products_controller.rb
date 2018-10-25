e3690851c9a368a7122517629603a2147d076987
Katrina Pritchard <pritckat@gmail.com>
2018-10-25 01:27:15 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,16 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product.new
   end
 
   def create
+    @product.create(product_params)
   end
 
+  private
+
+  def product_params
+    params.require(:product).permit()
+  end
 end
